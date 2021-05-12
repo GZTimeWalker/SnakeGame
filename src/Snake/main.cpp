ab538cfe82acd00ce14829e20e6404fedbeab8e7
GZTime <du_du_waw@163.com>
2021-05-13 04:13:08 +0800

    Add a way to turn on debug mode.

--- src/Snake/main.cpp
+++ src/Snake/main.cpp
@@ -5,18 +5,20 @@
 
 using namespace GZ;
 
-int main()
+int main(int argc, char* argv[])
 {
     Utils::Init();
-    //try
-    //{
+    if (argc > 1 && strcmp(argv[1], "-d"))
+        Utils::DEBUG = true;
+    try
+    {
         Game* game = new Game();
         game->Main();
         delete game; // May not be used because of the loop.
-    //}
-    //catch (std::exception& e)
-    //{
-    //    MessageBox(NULL, (LPCWSTR)e.what(), TEXT("已触发异常"), MB_OK);
-    //}
+    }
+    catch (std::exception& e)
+    {
+        MessageBox(NULL, (LPCWSTR)e.what(), TEXT("已触发异常"), MB_OK);
+    }
     return 0;
 }
